c72a25bb24fd37994801aafb725dce32749d83ff
Zanco1993 <zanconatofabio@gmail.com>
2021-12-09 16:14:00 +0100

    rinominato una classe

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 // creo le 2 variabili
 // seleziono la classe "fa-bars" ed al click
 // aggiungi la classe active che è diplay block
-const element = document.querySelector('.fa-bars');
+const openHamburger = document.querySelector('.fa-bars');
 const hamburger = document.querySelector('.hamburger-menu');
 
-element.addEventListener('click', function(){
+openHamburger.addEventListener('click', function(){
     hamburger.classList.add("active"); 
 })
 
